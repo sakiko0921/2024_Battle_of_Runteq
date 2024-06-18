03be3b696bbe110c7f561e10d1e71ccecbe53b30
sakiko0921 <kodasakiko@gmail.com>
2024-06-18 20:30:50 +0900

    画像キャッシュのパスを変更

--- app/uploaders/post_uploader.rb
+++ app/uploaders/post_uploader.rb
@@ -43,7 +43,7 @@ class PostUploader < CarrierWave::Uploader::Base
   end
 
   def cache_dir
-    "#{Rails.root}/tmp/uploads"
+    "/tmp/uploads"
   end
 
   # Override the filename of the uploaded files:
